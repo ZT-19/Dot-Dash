001fb434260754c628531685ba138f83674cf64e
Zhaorong Tu <zhaorongtuzt7@gmail.com>
2024-10-26 15:29:35 -0400

    Initial Commit

---
+++ Dot-Dash/Dot_DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dot_DashApp.swift
+//  Dot-Dash
+//
+//  Created by Zhaorong Tu on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Dot_DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
