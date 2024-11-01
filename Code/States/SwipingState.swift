d9fcce6941ddce3395f2ca9742b990d993a873b2
justinc918 <justinc918@g.ucla.edu>
2024-11-01 16:18:55 -0700

    swipingstate: defined directions to move in for touch controls

--- Code/States/SwipingState.swift
+++ Code/States/SwipingState.swift
@@ -10,16 +10,24 @@ import GameplayKit
 class SwipingState: GKState {
     unowned let scene: GameScene
     unowned let context: GameContext
-    
+
     private var isSwiping = false
     private var lastPosition: CGPoint = .zero
-    
+    private var firstPosition: CGPoint = .zero
+    enum swipeDirections {
+        case up
+        case down
+        case left
+        case right
+        case none
+    }
+
     init(scene: GameScene, context: GameContext) {
         self.scene = scene
         self.context = context
         super.init()
     }
-    
+
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         stateClass is SlidingState.Type || stateClass is GameOverState.Type
     }
@@ -30,46 +38,36 @@ class SwipingState: GKState {
     }
 
     func handleTouch(_ touch: UITouch) {
-        lastPosition = touch.location(in: scene)
-        if let fruitNode {
-            let allowedRange = context.layoutInfo.getSwipingRange(for: fruitNode, inBox: scene.boxNode)
-            if lastPosition.x < allowedRange.lowerBound {
-                fruitNode.position.x = allowedRange.lowerBound
-                scene.targetLineNode.position.x = allowedRange.lowerBound
-            } else if lastPosition.x > allowedRange.upperBound {
-                fruitNode.position.x = allowedRange.upperBound
-                scene.targetLineNode.position.x = allowedRange.upperBound
-            } else {
-                fruitNode.position.x = lastPosition.x
-                scene.targetLineNode.position.x = lastPosition.x
-            }
-        } else {
-            fruitNode?.position.x = lastPosition.x
-        }
+        firstPosition = touch.location(in: scene)
 
     }
 
     func handleTouchMoved(_ touch: UITouch) {
-        guard let fruitNode else {
-            return
-        }
+
         isSwiping = true
-        let newPosition = touch.location(in: scene)
-        let deltaX = newPosition.x - lastPosition.x
-        let updatedPositionX = fruitNode.position.x + deltaX
-        let allowedRange = context.layoutInfo.getSwipingRange(for: fruitNode, inBox: scene.boxNode)
-        let clampedPositionX = max(allowedRange.lowerBound, min(updatedPositionX, allowedRange.upperBound))
-        fruitNode.position.x = clampedPositionX
-        scene.targetLineNode.position.x = clampedPositionX
-        lastPosition = newPosition
+        lastPosition = touch.location(in: scene)
     }
 
     func handleTouchEnd() {
-        fruitNode?.enablePhysics()
-        context.stateMachine?.enter(SlidingState.self)
-        if let SlidingState = context.stateMachine?.state(forClass: SlidingState.self) {
-            SlidingState.fallingFruit = fruitNode
-            scene.targetLineNode.isHidden = true
+
+        let deltaX = lastPosition.x - firstPosition.x
+        let deltaY = lastPosition.y - firstPosition.y
+        let direction = swipeDirections.none
+        if deltaX > 0 && abs(deltaX) > abs(deltaY) {
+            let direction = swipeDirections.right
+        } else if deltaX < 0 && abs(deltaX) > abs(deltaY) {
+            let direction = swipeDirections.left
+        } else if deltaY > 0 && abs(deltaX) < abs(deltaY) {
+            let direction = swipeDirections.up
+        } else if deltaY < 0 && abs(deltaX) < abs(deltaY) {
+            let direction = swipeDirections.down
+        }
+
+        if direction != swipeDirections.none {
+            context.stateMachine?.enter(SlidingState.self)
+            if let SlidingState = context.stateMachine?.state(forClass: SlidingState.self) {
+                SlidingState.fallingFruit = fruitNode
+            }
         }
     }
 
