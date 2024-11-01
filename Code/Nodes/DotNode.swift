4e16b3763c0c6258120110cbdfde5e399dfdb143
justinc918 <justinc918@g.ucla.edu>
2024-11-01 16:28:37 -0700

    removed DDBoxNode because what is it used for again?

--- Code/Nodes/DotNode.swift
+++ Code/Nodes/DotNode.swift
@@ -12,12 +12,16 @@ class FruitNode: SKNode {
     let texture: SKTexture
     var radius: CGFloat  // I'll keep these 2 params in case we want to play with them
     var scale: CGFloat  // after the MVP is done
+    var disabled: Bool
+    var current: Bool
 
     init(texture: SKTexture, scale: CGFloat = 1.0) {
 
         self.texture = texture
         self.scale = scale
         self.radius = 0.0
+        disabled = false // already collected
+        current = false // dot we are currently on
         super.init()
         setup(type: type)
     }
@@ -30,7 +34,9 @@ class FruitNode: SKNode {
         let fruitShape = SKSpriteNode(texture: texture)
         fruitShape.setScale(scale)
         radius = fruitShape.size.width / 2.0
+        disabled = false
         addChild(fruitShape)
+        current = false
 
     }
 
